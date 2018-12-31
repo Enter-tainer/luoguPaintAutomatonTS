9e3346f9c4ffc8271682a4e8e7b6f5b2a70e384b
Margatroid <351652632@qq.com>
2018-12-31 22:35:23 +0800

    fix: fix bugs on linux

--- src/io/readBase32Image.ts
+++ src/io/readBase32Image.ts
@@ -3,7 +3,7 @@ import Base32Image from "../classes/Base32Image";
 
 function readBase32Image(filePath: string): Base32Image {
   const data = fs.readFileSync(filePath, "utf-8");
-  const lines: string[] = data.split("\r\n");
+  const lines: string[] = data.split("\n");
   const res = new Base32Image(lines);
   return res;
 }
